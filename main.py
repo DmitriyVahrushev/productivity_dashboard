e5bee53652ae7fac2f40ec89ef57ffdfccd1ae70
DmitriyVahrushev <notmybestmail@yandex.ru>
2020-05-13 17:42:11 +0300

    some minor fixes

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+# здесь будут браться значения из базы данных в будущем
+
+activity_list = [ 
+    100, 150, 240, 200, 150
+ ] 
+
 @app.route('/')
 def hello():
-    return render_template('main.html', title = "Main page") 
+    return render_template('main.html', title = "Main page", activity_list = activity_list) 
+
+@app.route('/pomodoro')
+def pomodoro_timer():
+    return render_template('pomodoro.html', title = "Pomodoro")
 
 if __name__ == '__main__':
     app.run(debug=True)
